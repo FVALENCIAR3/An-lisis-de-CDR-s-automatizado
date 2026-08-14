2f2d06dd16fdd3a2214342904596aae7a216bdc0
FVALENCIAR3 <fabian_valencia21@hotmail.com>
2026-08-14 14:57:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,11 +48,11 @@ for _key in [
         st.session_state[_key] = False
 
 
-st.title("🕵️‍♂️ Análisis de CDRs Automatizado")
+st.title("🕵️‍♂️ Análisis de CDR´s  Automatizado")
 st.caption(
-    "Análisis de inteligencia que permite limpiar, organizar y analizar "
-    "bases de datos CDR, con herramientas geográficas, espacio-temporales "
-    "y de perfilamiento."
+    " Análisis de inteligencia artificial que permite limpiar, organizar y analizar las bases de datos en archivos consolidados! "
+    " convierte celdas hexadecimales y excluye números de plataformas, también representa gráficamente en el mapa, "
+    " genera la sinopsis del comportamiento, zona de mayor permanencia, residencia o trabajo habitual del objetivo"
 )
 
 
